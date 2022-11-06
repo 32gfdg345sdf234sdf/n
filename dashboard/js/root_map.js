a62aab0bacf8e1a7c252ef6ff2b766c179c475c0
32gfdg345sdf234sdf <110407067+32gfdg345sdf234sdf@users.noreply.github.com>
2022-11-06 21:54:25 +0100

    Add files via upload

---
+++ dashboard/js/root_map.js
@@ -0,0 +1,36 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+var ipsJavascriptMap={"core":{"global_core":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/global_global_core.js","admin_core":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_core.js","admin_system":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_system.js","admin_dashboard":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_dashboard.js","admin_members":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_members.js","admin_templates":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_templates.js","admin_customization":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_customization.js","admin_stats":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_stats.js","admin_marketplace":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_marketplace.js","admin_support":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_support.js","admin_files":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_files.js","admin_settings":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/admin_admin_settings.js","front_core":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_core.js","front_widgets":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_widgets.js","front_streams":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_streams.js","front_statuses":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_statuses.js","front_system":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_system.js","front_profile":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_profile.js","front_search":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_search.js","front_ignore":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_ignore.js","front_messages":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_messages.js","front_modcp":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_core/front_front_modcp.js"},"bimchatbox":{"front_chatbox":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_bimchatbox/front_front_chatbox.js"},"forums":{"front_forum":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_forums/front_front_forum.js","front_topic":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_forums/front_front_topic.js","admin_settings":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_forums/admin_admin_settings.js"},"nexus":{"front_store":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/front_front_store.js","front_checkout":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/front_front_checkout.js","front_support":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/front_front_support.js","front_subscriptions":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/front_front_subscriptions.js","global_gateways":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/global_global_gateways.js","admin_store":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/admin_admin_store.js","admin_support":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/admin_admin_support.js","admin_notifications":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_nexus/admin_admin_notifications.js"},"cms":{"admin_pages":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_cms/admin_admin_pages.js","admin_databases":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_cms/admin_admin_databases.js","admin_blocks":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_cms/admin_admin_blocks.js"},"brilliantdiscord":{"admin_config":"https://web.archive.org/web/20211229035023/https://diablo.pub/forums/uploads/javascript_brilliantdiscord/admin_admin_config.js"}};;
+
+}
+/*
+     FILE ARCHIVED ON 03:50:23 Dec 29, 2021 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 21:28:50 Nov 05, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 831.757
+  exclusion.robots: 0.106
+  exclusion.robots.policy: 0.1
+  cdx.remote: 0.091
+  esindex: 0.009
+  LoadShardBlock: 544.635 (3)
+  PetaboxLoader3.datanode: 229.765 (4)
+  CDXLines.iter: 18.516 (3)
+  load_resource: 107.79
+  PetaboxLoader3.resolve: 36.047
+*/
